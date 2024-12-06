59907e52b12c14315a0a3e369e901cd7ff34bbf2
LNXDesman <desman@desman.xyz>
2024-12-06 11:26:53 +0100

    Optimize code

--- 05/day05.go
+++ 05/day05.go
@@ -20,6 +20,8 @@ func main() {
 
 	testPages(input)
 	rectify(input)
+	testPages02(input)
+	rectify02(input)
 }
 
 func timerHighRes(name string) func() {
@@ -34,11 +36,11 @@ func timerHighRes(name string) func() {
 	}
 }
 
-func rectify(input []string) {
-	defer timerHighRes("Day 5 2 rectify")()
+func rectify02(input []string) {
+	defer timerHighRes("Day 5 2 rectify optimized")()
 	count := 0
 	isPart1 := true
-	rules := [][]int{}
+	rules := make(map[int]map[int]bool)
 	for i := range input {
 		line := input[i]
 		if line == "" {
@@ -49,17 +51,19 @@ func rectify(input []string) {
 			if err != nil {
 				panic(err)
 			}
-			rules = append(rules, []int{a, b})
-
+			if rules[a] == nil {
+				rules[a] = make(map[int]bool)
+			}
+			rules[a][b] = true
 		} else {
 			pages := strings.Split(line, ",")
-			if pos := condMiddle(rules, pages); pos != -1 {
+			if pos := findError02(rules, pages); pos != -1 {
 				//fmt.Printf("rectify %v at %v\n", line, pos)
 				for true {
 					pages[pos], pages[pos-1] = pages[pos-1], pages[pos]
 					//fmt.Printf("try: %v at %v\n", pages, pos)
 
-					if pos = condMiddle(rules, pages); pos == -1 {
+					if pos = findError02(rules, pages); pos == -1 {
 						//fmt.Printf("rectified by: %v)\n", pages)
 						middle, err := strconv.Atoi(pages[len(pages)/2])
 						if err != nil {
@@ -75,8 +79,86 @@ func rectify(input []string) {
 	fmt.Printf("%v\n", count)
 }
 
+func rectify(input []string) {
+	defer timerHighRes("Day 5 2 rectify not optimized")()
+	count := 0
+	isPart1 := true
+
+	rules := [][]int{}
+	for i := range input {
+		line := input[i]
+		if line == "" {
+			isPart1 = false
+		} else if isPart1 == true {
+			a, err := strconv.Atoi(strings.Split(line, "|")[0])
+			b, err := strconv.Atoi(strings.Split(line, "|")[1])
+			if err != nil {
+				panic(err)
+			}
+			rules = append(rules, []int{a, b})
+		} else {
+			pages := strings.Split(line, ",")
+			if pos := findError(rules, pages); pos != -1 {
+				//fmt.Printf("rectify %v at %v\n", line, pos)
+				for true {
+					pages[pos], pages[pos-1] = pages[pos-1], pages[pos]
+					//fmt.Printf("try: %v at %v\n", pages, pos)
+
+					if pos = findError(rules, pages); pos == -1 {
+						//fmt.Printf("rectified by: %v)\n", pages)
+						middle, err := strconv.Atoi(pages[len(pages)/2])
+						if err != nil {
+							panic(err)
+						}
+						count += middle
+						break
+					}
+				}
+			}
+		}
+	}
+	fmt.Printf("%v\n", count)
+}
+
+func testPages02(input []string) {
+	defer timerHighRes("Day 5 1 testPages optimized")()
+	count := 0
+	isPart1 := true
+	rules := make(map[int]map[int]bool)
+
+	for i := range input {
+		line := input[i]
+		if line == "" {
+			isPart1 = false
+		} else if isPart1 == true {
+			a, err := strconv.Atoi(strings.Split(line, "|")[0])
+			b, err := strconv.Atoi(strings.Split(line, "|")[1])
+
+			if err != nil {
+				panic(err)
+			}
+			if rules[a] == nil {
+				rules[a] = make(map[int]bool)
+			}
+			rules[a][b] = true
+		} else {
+			pages := strings.Split(line, ",")
+			middle, err := strconv.Atoi(pages[len(pages)/2])
+			if err != nil {
+				panic(err)
+			}
+			if findError02(rules, pages) == -1 {
+				count += middle
+			}
+
+		}
+
+	}
+	fmt.Printf("%v\n", count)
+}
+
 func testPages(input []string) {
-	defer timerHighRes("Day 5 1 testPages")()
+	defer timerHighRes("Day 5 1 testPages not optimized")()
 	count := 0
 	isPart1 := true
 	rules := [][]int{}
@@ -98,7 +180,7 @@ func testPages(input []string) {
 			if err != nil {
 				panic(err)
 			}
-			if condMiddle(rules, pages) == -1 {
+			if findError(rules, pages) == -1 {
 				count += middle
 			}
 
@@ -108,7 +190,62 @@ func testPages(input []string) {
 	fmt.Printf("%v\n", count)
 }
 
-func condMiddle(rules [][]int, pages []string) int {
+func findError02(rules map[int]map[int]bool, pages []string) int {
+	oldpages := make([]int, 0, len(pages))
+	for x, pagestr := range pages {
+		page, err := strconv.Atoi(pagestr)
+		if err != nil {
+			panic(err)
+		}
+		for _, oldpage := range oldpages {
+
+			if rules[page] != nil && rules[page][oldpage] {
+				return x
+			}
+		}
+		oldpages = append(oldpages, page)
+	}
+	return -1
+}
+
+func findErrorO1(rules map[int]map[int]bool, pages []string) int {
+	//oldpages := []int{}
+	checked := make(map[int]bool)
+	for x, pagestr := range pages {
+		page, err := strconv.Atoi(pagestr)
+		if err != nil {
+			panic(err)
+		}
+		if dep, exists := rules[page]; exists {
+			for d := range dep {
+				if checked[d] {
+					return x
+				}
+			}
+		}
+		checked[page] = true
+	}
+	return -1
+}
+
+func findErrorO0(rules [][]int, pages []string) int {
+	checked := make(map[int]bool)
+	for x, pagestr := range pages {
+		page, err := strconv.Atoi(pagestr)
+		if err != nil {
+			panic(err)
+		}
+		for _, rule := range rules {
+			if rule[0] == page && checked[rule[1]] {
+				return x
+			}
+		}
+		checked[page] = true
+	}
+	return -1
+}
+
+func findError(rules [][]int, pages []string) int {
 	oldpages := []int{}
 	for x := range pages {
 		page, err := strconv.Atoi(pages[x])
